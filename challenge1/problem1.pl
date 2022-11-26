dbac70dd9934372c27c005ccf8eb513cca3d735c
CescHolly <2012.1c.holfrancesc@gmail.com>
2022-11-26 20:48:38 +0100

    Challenge1: Prolog program.

---
+++ challenge1/problem1.pl
@@ -0,0 +1,260 @@
+symbolicOutput(0).  % set to 1 to see symbolic output only; 0 otherwise.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% To use this prolog template for other optimization problems, replace the code parts 1,2,3,4 below. %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%% INPUT:
+
+:-[prolog_input_challenge_1].
+
+%%%%%%%%%%%%%%%%%%%%% END INPUT. %%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%% Some helpful definitions to make the code cleaner:
+
+machine(M) :- machineTasks(M,_).
+task(T) :- taskDuration(T,_).
+hour(H) :- maxHourInput(MH), between(1,MH,H).
+
+preceedes(T,S) :- orders(_,L), nth1(X,L,T), nth1(Y,L,S), X<Y.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% 1.- Declare SAT variables to be used
+
+satVariable( assignation(T,H) ):- task(T), hour(H). % Means task T starts at hour H at its machine.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. This predicate writeClauses(MaxCost) generates the clauses that guarantee that
+% a solution with cost at most MaxCost is found
+
+writeClauses(infinite):- !, maxHourInput(MaxHour), writeClauses(MaxHour),!.
+writeClauses(MaxHour):-
+    tasksInsideTimeWindow(MaxHour),
+    tasksOfSameOrderSorted(MaxHour),
+    nonOverlappingTasksByMachine(MaxHour),
+    tasksGetAssignedOnce(MaxHour),
+    true,!.
+writeClauses(_):- told, nl, write('writeClauses failed!'), nl,nl, halt.
+
+nonOverlappingTasksByMachine(MaxHour):-
+    machine(M),
+    machineTasks(M,L),
+    task(T1),
+    task(T2),
+    T1 \= T2,
+    member(T1,L),
+    member(T2,L),
+    between(1,MaxHour,H),
+    taskDuration(T1,D),
+    EndT1 is H+D,
+    Max1 is MaxHour+1,
+    EndMaxT1 is min(EndT1, Max1),
+    taskAtHIncompatibleWithTaskFromIToF(T1, H, T2, H, EndMaxT1),
+    fail.
+nonOverlappingTasksByMachine(_).
+
+tasksOfSameOrderSorted(MaxHour):-
+    task(T1),
+    task(T2),
+    preceedes(T1,T2),
+    between(1,MaxHour,H),
+    taskDuration(T1,D),
+    EndT1 is H+D,
+    Max1 is MaxHour+1,
+    EndMaxT1 is min(EndT1, Max1),
+    taskAtHIncompatibleWithTaskFromIToF(T1, H, T2, 1, EndMaxT1),
+    fail.
+tasksOfSameOrderSorted(_).
+
+taskAtHIncompatibleWithTaskFromIToF(T1, H, T2, I, F) :-
+    I<F,
+    writeClause([-assignation(T1,H), -assignation(T2,I)]),
+    I1 is I+1,
+    taskAtHIncompatibleWithTaskFromIToF(T1, H, T2, I1, F).
+taskAtHIncompatibleWithTaskFromIToF(_,_,_,F,F).
+
+tasksInsideTimeWindow(MaxHour) :- 
+    task(T),
+    taskDuration(T,D),
+    BackDist is MaxHour+1-D,
+    Max1 is MaxHour+1,
+    writeNegLateTasks(T, BackDist, Max1),
+    fail.
+tasksInsideTimeWindow(_).
+
+writeNegLateTasks(T, H, MAX):-
+    H < MAX,
+    writeClause([-assignation(T,H)]),
+    H1 is H+1,
+    writeNegLateTasks(T,H1,MAX).
+writeNegLateTasks(_,MAX,MAX).
+
+
+tasksGetAssignedOnce(MaxHour):-
+    task(T),
+    findall( assignation(T,H), between(1,MaxHour,H), Lits ),
+    exactly(1,Lits),
+    fail.
+tasksGetAssignedOnce(_).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 3. This predicate displays a given solution M:
+
+displaySol(Model):-
+    machineTasks(M,LM),
+    write('Machine: '), write(M), nl,
+    task(T), member(T,LM),
+    write('Task: '), write(T), write(' '),
+    member( assignation(T,H), Model ),
+    taskDuration(T,D),
+    EndT is H+D-1,
+    write(H), write('-'), write(EndT), nl, fail.
+displaySol(_):- nl,nl,!.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 4. This predicate computes the cost of a given solution M:
+    
+costOfThisSolution(M, Cost):-
+    findall(EndT, (member(assignation(T,H),M), taskDuration(T,D), EndT is H+D-1), FinishingHours),
+    sort(FinishingHours, SFH),
+    reverse(SFH, [Cost|_]),
+    write('Cost is: ....... '), write(Cost), nl.
+    
+% precondition: elements in list are ordered in increasing value
+maximumDifferenceBetweenConsecutiveElems([],0).
+maximumDifferenceBetweenConsecutiveElems([_],0).
+maximumDifferenceBetweenConsecutiveElems([A,B|L],N):-
+    maximumDifferenceBetweenConsecutiveElems([B|L],N),
+    N > B-A, !.
+maximumDifferenceBetweenConsecutiveElems([A,B|_],N):-
+    N is B-A.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% No need to modify anything below this line:
+
+main:-  symbolicOutput(1), !, writeClauses(infinite), halt.   % print the clauses in symbolic form and halt
+main:-
+    told, write('Looking for initial solution with arbitrary cost...'), nl,
+    initClauseGeneration,
+    tell(clauses), writeClauses(infinite), told,
+    tell(header),  writeHeader,  told,
+    numVars(N), numClauses(C), 
+    write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
+    shell('cat header clauses > infile.cnf',_),
+    write('Launching minisat...'), nl,
+    shell('minisat infile.cnf model', Result),  % if sat: Result=10; if unsat: Result=20.
+    treatResult(Result,[]),!.
+
+treatResult(20,[]       ):- write('No solution exists.'), nl, halt.
+treatResult(20,BestModel):-
+    nl,costOfThisSolution(BestModel,Cost), write('Unsatisfiable. So the optimal solution was this one with cost '),
+    write(Cost), write(':'), nl, displaySol(BestModel), 
+    write('%% END OF FOUND SOLUTION %%'),
+    nl,nl,halt.
+treatResult(10,_):- %   shell('cat model',_),
+    nl,write('Solution found '), flush_output,
+    see(model), symbolicModel(M), seen,
+    costOfThisSolution(M,Cost),
+    write('with cost '), write(Cost), nl,nl,
+    displaySol(M), 
+    Cost1 is Cost-1,   nl,nl,nl,nl,nl,  write('Now looking for solution with cost '), write(Cost1), write('...'), nl,
+    initClauseGeneration, tell(clauses), writeClauses(Cost1), told,
+    tell(header),  writeHeader,  told,
+    numVars(N),numClauses(C),
+    write('Generated '), write(C), write(' clauses over '), write(N), write(' variables. '),nl,
+    shell('cat header clauses > infile.cnf',_),
+    write('Launching minisat...'), nl,
+    shell('minisat infile.cnf model', Result),  % if sat: Result=10; if unsat: Result=20.
+    treatResult(Result,M),!.
+treatResult(_,_):- write('cnf input error. Wrote something strange in your cnf?'), nl,nl, halt.
+    
+
+initClauseGeneration:-  %initialize all info about variables and clauses:
+	retractall(numClauses(   _)),
+	retractall(numVars(      _)),
+	retractall(varNumber(_,_,_)),
+	assert(numClauses( 0 )),
+	assert(numVars(    0 )),     !.
+
+writeClause([]):- symbolicOutput(1),!, nl.
+writeClause([]):- countClause, write(0), nl.
+writeClause([Lit|C]):- w(Lit), writeClause(C),!.
+w(-Var):- symbolicOutput(1), satVariable(Var), write(-Var), write(' '),!. 
+w( Var):- symbolicOutput(1), satVariable(Var), write( Var), write(' '),!. 
+w(-Var):- satVariable(Var),  var2num(Var,N),   write(-), write(N), write(' '),!.
+w( Var):- satVariable(Var),  var2num(Var,N),             write(N), write(' '),!.
+w( Lit):- told, write('ERROR: generating clause with undeclared variable in literal '), write(Lit), nl,nl, halt.
+
+
+% given the symbolic variable V, find its variable number N in the SAT solver:
+var2num(V,N):- hash_term(V,Key), existsOrCreate(V,Key,N),!.
+existsOrCreate(V,Key,N):- varNumber(Key,V,N),!.                            % V already existed with num N
+existsOrCreate(V,Key,N):- newVarNumber(N), assert(varNumber(Key,V,N)), !.  % otherwise, introduce new N for V
+
+writeHeader:- numVars(N),numClauses(C), write('p cnf '),write(N), write(' '),write(C),nl.
+
+countClause:-     retract( numClauses(N0) ), N is N0+1, assert( numClauses(N) ),!.
+newVarNumber(N):- retract( numVars(   N0) ), N is N0+1, assert(    numVars(N) ),!.
+
+% Getting the symbolic model M from the output file:
+symbolicModel(M):- get_code(Char), readWord(Char,W), symbolicModel(M1), addIfPositiveInt(W,M1,M),!.
+symbolicModel([]).
+addIfPositiveInt(W,L,[Var|L]):- W = [C|_], between(48,57,C), number_codes(N,W), N>0, varNumber(_,Var,N),!.
+addIfPositiveInt(_,L,L).
+readWord( 99,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), readWord(Ch1,W),!. % skip line starting w/ c
+readWord(115,W):- repeat, get_code(Ch), member(Ch,[-1,10]), !, get_code(Ch1), readWord(Ch1,W),!. % skip line starting w/ s
+readWord(-1,_):-!, fail. %end of file
+readWord(C,[]):- member(C,[10,32]), !. % newline or white space marks end of word
+readWord(Char,[Char|W]):- get_code(Char1), readWord(Char1,W), !.
+:-dynamic(varNumber / 3).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Express that Var is equivalent to the disjunction of Lits:
+expressOr( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> or('), write(Lits), write(')'), nl, !. 
+expressOr( Var, Lits ):- member(Lit,Lits), negate(Lit,NLit), writeClause([ NLit, Var ]), fail.
+expressOr( Var, Lits ):- negate(Var,NVar), writeClause([ NVar | Lits ]),!.
+
+% Express that Var is equivalent to the conjunction of Lits:
+expressAnd( Var, Lits) :- symbolicOutput(1), write( Var ), write(' <--> and('), write(Lits), write(')'), nl, !. 
+expressAnd( Var, Lits):- member(Lit,Lits), negate(Var,NVar), writeClause([ NVar, Lit ]), fail.
+expressAnd( Var, Lits):- findall(NLit, (member(Lit,Lits), negate(Lit,NLit)), NLits), writeClause([ Var | NLits]), !.
+
+
+%%%%%% Cardinality constraints on arbitrary sets of literals Lits:
+
+exactly(K,Lits):- symbolicOutput(1), write( exactly(K,Lits) ), nl, !.
+exactly(K,Lits):- atLeast(K,Lits), atMost(K,Lits),!.
+
+atMost(K,Lits):- symbolicOutput(1), write( atMost(K,Lits) ), nl, !.
+atMost(K,Lits):-   % l1+...+ln <= k:  in all subsets of size k+1, at least one is false:
+	negateAll(Lits,NLits),
+	K1 is K+1,    subsetOfSize(K1,NLits,Clause), writeClause(Clause),fail.
+atMost(_,_).
+
+atLeast(K,Lits):- symbolicOutput(1), write( atLeast(K,Lits) ), nl, !.
+atLeast(K,Lits):-  % l1+...+ln >= k: in all subsets of size n-k+1, at least one is true:
+	length(Lits,N),
+	K1 is N-K+1,  subsetOfSize(K1, Lits,Clause), writeClause(Clause),fail.
+atLeast(_,_).
+
+negateAll( [], [] ).
+negateAll( [Lit|Lits], [NLit|NLits] ):- negate(Lit,NLit), negateAll( Lits, NLits ),!.
+
+negate( -Var,  Var):-!.
+negate(  Var, -Var):-!.
+
+subsetOfSize(0,_,[]):-!.
+subsetOfSize(N,[X|L],[X|S]):- N1 is N-1, length(L,Leng), Leng>=N1, subsetOfSize(N1,L,S).
+subsetOfSize(N,[_|L],   S ):-            length(L,Leng), Leng>=N,  subsetOfSize( N,L,S).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
